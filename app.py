cd8a2b9ee4e6ae4278d7e3bc914804ec482b9364
Chih-Chao <86564700+imchihchao@users.noreply.github.com>
2025-05-16 22:48:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import gradio as gr
-from fastapi import FastAPI
 
-# 您的 Gradio 介面定義
 def greet(name):
     return "Hello " + name + "!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+def main():
+    gr.Interface(fn=greet, inputs="text", outputs="text").launch()
 
-# 建立一個 FastAPI 應用程式並掛載 Gradio 應用程式
-app = FastAPI()
-app = gr.mount_gradio_app(app, iface, path="/") # 將 Gradio 掛載到根路徑
-
-# 注意：在 Vercel 環境中，您不需要呼叫 iface.launch()
+if __name__ == '__main__':
+	main()
