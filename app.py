613dc645b066af43f9624d68c5ac8d91e0631682
Chih-Chao <86564700+imchihchao@users.noreply.github.com>
2025-06-09 10:54:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "message": "Hello from FastAPI!"})
+
+@app.post("/hello")
+async def hello():
+    return {"name": 'world'}
+
